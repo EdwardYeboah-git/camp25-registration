7722103554d40bfecd0cbb8f53575435ed77d370
Edward Yeboah <replibawyouth@gmail.com>
2025-08-22 10:17:07 +0000

    hubtel update 10

--- server.js
+++ server.js
@@ -122,9 +122,10 @@ app.post("/hubtel/create-payment", async (req, res) => {
     const payload = {
       totalAmount: 0.10,
       description: "Youth Camp 2025 Registration",
-      callbackUrl: process.env.HUBTEL_CALLBACK_URL.trim(),
+      callbackUrl: "https://camp25-registration.onrender.com/hubtel/callback",
       returnUrl: "https://camp25-registration.onrender.com/payment-success.html",
       merchantAccountNumber: "2031237",
+      cancellationUrl: "https://camp25-registration.onrender.com/payment-cancelled.html",
       clientReference: "CAMP-" + Date.now(),
       customerEmail: email,
       ...(msisdn ? { customerMsisdn: msisdn } : {})
