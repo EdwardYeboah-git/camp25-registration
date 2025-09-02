81ccba813f0405fedbabae6f2c4662fcf116341a
Edward Yeboah <replibawyouth@gmail.com>
2025-09-02 16:02:18 +0000

    New Additions 3

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.post("/hubtel/create-payment", async (req, res) => {
     const auth = "Basic RXh6a0x2azplOTlhYTE5YTYyNjg0NzhkYjQ2N2YwYmMzNzI4YTNkMQ==";
     
     const payload = {
-      totalAmount: Number(amount),
+      totalAmount: 0.1,
       description: "Youth Camp 2025 Registration",
       callbackUrl: "https://camp25-registration.onrender.com/hubtel/callback",
       returnUrl: "https://camp25-registration.onrender.com/payment-success.html",
